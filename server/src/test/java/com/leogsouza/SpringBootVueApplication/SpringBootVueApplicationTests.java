7ae0deb0dd4b80c507b626c40a90ce2fd690ab07
Leonardo Souza <leogsouza@gmail.com>
2020-02-09 17:41:16 -0300

    Add Spring Boot project

---
+++ server/src/test/java/com/leogsouza/SpringBootVueApplication/SpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leogsouza.SpringBootVueApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
