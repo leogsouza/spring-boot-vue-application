95db095297fa291d82362e89d72c898b1f6b578f
Leonardo Souza <leogsouza@gmail.com>
2020-02-12 15:09:45 -0300

    Add CORS

--- server/src/main/java/com/leogsouza/springbootvue/repository/TodoRepository.java
+++ server/src/main/java/com/leogsouza/springbootvue/repository/TodoRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-interface TodoRepository extends JpaRepository<Todo, Long> {}
+public interface TodoRepository extends JpaRepository<Todo, Long> {}
